c3f4aa2795ee06edd7604b1c5048d4cdb6068b48
Tofik Sahraoui <tofiksa@gmail.com>
2022-01-10 03:09:05 +0100

    [FEAT] - CSS animation ADD - initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div id="container">
+          Hvem sa
+        <div id="flip">
+          <div><div>hva</div></div>
+          <div><div>hvor</div></div>
+          <div><div>hvem</div></div>
+        </div>
+        ?
+      </div>
+
+
     </div>
   );
 }
