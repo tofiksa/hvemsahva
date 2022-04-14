3d7b3ac9221e0c2c3aa7430fa48c78dd8d0ffdcc
Tofik Sahraoui <tofiksa@gmail.com>
2022-04-14 15:36:59 +0200

    [FEAT] lagt til flere spørsmål

--- src/templates/FrontPage.js
+++ src/templates/FrontPage.js
@@ -3,7 +3,6 @@ import { Header } from '../components/Header.js';
 import { AnimationWidget } from '../components/AnimationWidget.js';
 import { SporsmaalSkjema } from '../components/SporsmaalSkjema.js';
 import { Personalia } from '../model/personaliaJson.js';
-import { Footer } from '../components/Footer.js';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -11,14 +10,13 @@ import 'survey-react/modern.min.css';
 import { StylesManager } from 'survey-react';
 
 
-StylesManager.applyTheme("modern");
+
 
 export const FrontPage = () => {
 
     return(
         <div id="container">
             <div><Header/></div>
-            <div id="content">
             <Container>
                 <Row>
                     <Col></Col>
@@ -30,8 +28,7 @@ export const FrontPage = () => {
                     <Col></Col>
                 </Row>
             </Container>
-                </div>
-            <div><Footer/></div>
         </div>
+        
     );
 }
