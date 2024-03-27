e67437a1fc8f8e2aa30dc91ac07a57b05587b649
Tofik Sahraoui <tofiksa@gmail.com>
2024-03-27 20:58:32 +0100

    new start

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json());
 app.use(morgan('combined'));
 app.use(cors())
 
+const PORT = process.env.PORT || 3000;
+
 const supabase = createClient(
   'https://kdhaitlddslpxphpcnvc.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkaGFpdGxkZHNscHhwaHBjbnZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExODI3ODksImV4cCI6MjAyNjc1ODc4OX0.dbS0y-hKuH3m5fGdtX_eeYKVYKdiGs2iVf61EUmMkH4'
@@ -33,13 +35,15 @@ app.post('/api/start', async (req, res) => {
     res.send(error);
 });
 
-app.listen(3001, () => {
-  console.log('Server is listening on port 3001');
-});
+
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../build')));
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`App is up and running. Listening on port ${PORT}`);
 });
