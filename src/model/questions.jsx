48269ced1cf2f465b9f74364c8f519ef22221422
Tofik Sahraoui <tofiksa@gmail.com>
2024-03-29 22:43:12 +0100

    layout 2

--- src/model/questions.jsx
+++ src/model/questions.jsx
@@ -1,13 +1,16 @@
 import { getImageUrlByNameFromLocalStorage } from '../services/retrieveFromStorage'
 
 const EASTERBUNNY = getImageUrlByNameFromLocalStorage('EasterEggHunt.gif');
-const LION = getImageUrlByNameFromLocalStorage('lion.jpeg');
 const RABIT = getImageUrlByNameFromLocalStorage('rabit.png');
 const CHICKEN = getImageUrlByNameFromLocalStorage('chicken.jpeg');
-const BEAR = getImageUrlByNameFromLocalStorage('bear.png');
+const KONGKIFOT = getImageUrlByNameFromLocalStorage('kongkifot.jpg');
+const FEILKONGKIFOT1 = getImageUrlByNameFromLocalStorage('feilkifot1.jpg');
+const FEILKONGKIFOT2 = getImageUrlByNameFromLocalStorage('feilkifot2.jpg');
+const FEILKONGKIFOT3 = getImageUrlByNameFromLocalStorage('feilkifot3.jpg');
 const RAMADAN = getImageUrlByNameFromLocalStorage('ramadan.jpeg');
 const MAN_PRAYING = getImageUrlByNameFromLocalStorage('manpraying.jpeg');
-const ALLAH = getImageUrlByNameFromLocalStorage('Allah.jpeg');
+const KONGKIFOTSLOTT =
+getImageUrlByNameFromLocalStorage('kongKifotSlott.png');
 
 export const personaliaJson = {
     "title": "Påskeeggjakt 2024",
@@ -17,11 +20,11 @@ export const personaliaJson = {
     "completedHtmlOnCondition": [{
         "expression": "{totalScore} > 29",
         "html":
-          "Gratulerer, du har fått nok poeng til å få ledetråder til påskeeggene, trykk på lenken: <a href=\"/#/clues\">her</a>"
+          "Gratulerer, du har fått nok poeng til å få ledetråder til påskeeggene, trykk på lenken: <a href=\"/clues\">her</a>"
       }, {
         "expression": "{totalScore} <= 30",
         "html":
-          "Du fikk {totalScore} poeng av {maxScore}. Du må klare minst 30 for å få ledetråd. trykk på lenken for å prøve igjen: <a href=\"/#/\">her</a>"
+          "Du fikk {totalScore} poeng av {maxScore}. Du må klare minst 30 for å få ledetråd. trykk på lenken for å prøve igjen: <a href=\"/\">her</a>"
       }],
     "pages": [{
         "name":"page1",
@@ -39,16 +42,16 @@ export const personaliaJson = {
                 "type": "html",
                 "name": "info",
                 
-                "html": "<img height=\"100\" width=\"170\" src=\""+ALLAH+"\"/>",
+                "html": "<img height=\"100\" width=\"170\" src=\""+KONGKIFOTSLOTT+"\"/>",
             },
             {
             "type": "radiogroup",
             "name": "arabiskbokstav",
-            "title": "Hva står det på arabisk?",
+            "title": "Hva heter landet Kong kifot er kongen av?",
             "choices": [
-                "Esma", "Josef", "Allah", "Hus"
+                "Bikuben", "Bikubbe", "Bikubia", "Bingen"
             ],
-            "correctAnswer": "Allah",
+            "correctAnswer": "Bikubia",
             "score": 5
         }],
     },{ "name":"page2",
@@ -58,7 +61,7 @@ export const personaliaJson = {
                 "type":"html",
                 "name":"info2",
                 "expression": "{totalScore}",
-                "html":"<img height=\"100\" width=\"170\" src=\""+LION+"\"/><legend/>"
+                "html":""
             },
             {
             "type": "ranking",
@@ -69,32 +72,36 @@ export const personaliaJson = {
                         "س",
                         "د",
                         "أ",
+                        "ل",
+                        "و",
+                        "ن",
+                        "ر",
                     ],
-                    "correctAnswer": ["أ","س","د"],
+                    "correctAnswer": ["أ","د","ر","س","ل","ن","و"],
                     "score": 5
         }],
     },{ "name":"page3",
     "elements": [{
         "type": "imagepicker",
                 "name": "choosepicture",
-                "title": "Hvilket dyr brukes iforbindelse med påsken?",
+                "title": "Velg korrekt bilde av kong Kifot",
                 "isRequired": true,
                 "choices": [
                     {
-                        "value": "lion",
-                        "imageLink": LION
+                        "value": "kongkifot",
+                        "imageLink": KONGKIFOT
                     }, {
-                        "value": "rabit",
-                        "imageLink": RABIT
+                        "value": "feilkifot3",
+                        "imageLink": FEILKONGKIFOT3
                     }, {
-                        "value": "chicken",
-                        "imageLink": CHICKEN
+                        "value": "feilkifot2",
+                        "imageLink": FEILKONGKIFOT2
                     }, {
-                        "value": "bear",
-                        "imageLink": BEAR
+                        "value": "feilkifot1",
+                        "imageLink": FEILKONGKIFOT1
                     }
                 ],
-                "correctAnswer": "rabit",
+                "correctAnswer": "kongkifot",
                 "score": 5
     }],
     },{ "name":"page4",
@@ -105,16 +112,16 @@ export const personaliaJson = {
     {
         "type": "html",
         "name": "info",
-        "html": "<div style=\"position: relative; overflow: hidden; padding-top: 56.25%;\"><iframe src=\"https://share.synthesia.io/embeds/videos/a2666ba5-f600-4081-92eb-6bae2b4c4519\" loading=\"lazy\" title=\"Synthesia video player - Your AI video\" allow=\"encrypted-media; fullscreen;\" style=\"position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0; overflow:hidden;\"></iframe></div>",
+        "html": "<div style=\"position: relative; overflow: hidden; padding-top: 56.25%;\"><iframe src=\"https://share.synthesia.io/embeds/videos/f98e0d6e-1437-4e71-b9c9-187cb7d889d0\" loading=\"lazy\" title=\"Synthesia video player - Your AI video\" allow=\"encrypted-media; fullscreen;\" style=\"position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0; overflow:hidden;\"></iframe></div>",
     },
     {
     "type": "radiogroup",
     "name": "arabisk",
-    "title": "Hva sier kvinnen på videoen?",
+    "title": "Hva sier mannen på videoen?",
     "choices": [
-        "Hei, hvordan går det?", "Hei, hvor er bussen?", "Hei, hva er klokken?", "Jeg vil sove"
+        "Hva heter du?", "Hei, hvor er bussen?", "Hva er klokken?", "Jeg vil sove"
     ],
-    "correctAnswer": "Hei, hvordan går det?",
+    "correctAnswer": "Hva heter du?",
     "score": 5
     }]
     },{ "name":"page5",
--- src/templates/CluesPage.tsx
+++ src/templates/CluesPage.tsx
@@ -5,93 +5,91 @@ import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
 
 const CluesPage: React.FC = () => {
+  const [text, setText] = useState('');
+  const [isValid, setIsValid] = useState(false);
 
-const [text, setText] = useState('');
-const [isValid, setIsValid] = useState(false);
-
-const iframeWrapperStyle: React.CSSProperties = {
+  const iframeWrapperStyle: React.CSSProperties = {
     position: 'relative',
     overflow: 'hidden',
     paddingTop: '56.25%',
-};
+  };
 
-const iframeStyle: React.CSSProperties = {
-position: 'absolute',
-width: '100%',
-height: '100%',
-top: '0',
-left: '0',
-border: 'none',
-padding: '0',
-margin: '0',
-};
+  const iframeStyle: React.CSSProperties = {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    top: '0',
+    left: '0',
+    border: 'none',
+    padding: '0',
+    margin: '0',
+  };
 
-const legendStyle: React.CSSProperties = {
-marginTop: '1rem',
-textAlign: 'left',
-fontWeight: 'bold',
-width: '50%'
-};
+  const legendStyle: React.CSSProperties = {
+    marginTop: '1rem',
+    textAlign: 'left',
+    fontWeight: 'bold',
+    width: '50%',
+  };
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsValid(checkText());
 
     if (checkText()) {
-        navigate('/egg1');
-      }
+      navigate('/egg1');
+    }
   };
 
   const checkText = () => {
-    return text.toLowerCase() === 'mamma' || text.toLowerCase() === 'mor';
+    return (
+      text.toLowerCase() === 'hanan' || text.toLowerCase() === 'tante hanan'
+    );
   };
 
-
-return (
-<>
-<Header />
-<div style={iframeWrapperStyle}>
-<iframe
-    src="https://share.synthesia.io/embeds/videos/88919f44-2193-4af8-b2a0-b91c98dbcf60"
-    loading="lazy"
-    title="Synthesia video player - Your AI video"
-    allow="encrypted-media; fullscreen;"
-    style={iframeStyle}
-></iframe>
-</div>
-<div style={legendStyle}>
-        Hun gir de beste klemmene og alltid er nær,
-        Hun gir deg kjærlighet, omsorg og beskyttelse så kjær.
-        Hun hjelper deg når du faller og trøster deg når du gråter,
-        Hun lager de beste måltidene, og når du er syk, så passer hun på deg hele natten.
-
-        Hvem er denne personen så god og så varm,
-        Som alltid vil være der for deg, og som aldri vil forsvinne eller gjøre deg harm?
-    </div>
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-    <Form onSubmit={handleSubmit} style={{ width: '50%' }}>
-    <Form.Group controlId="formText">
-        
-        <Form.Control
-            type="text"
-            placeholder="Skriv inn koden her"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            isInvalid={!isValid}
-        />
-        <Form.Control.Feedback type="invalid">
-            Feil kode
-        </Form.Control.Feedback>
-        </Form.Group>
-        <Button variant="primary" type="submit">Svar</Button>
-    </Form>
-    </div>
-
+  return (
+    <>
+      <Header />
+      <div style={iframeWrapperStyle}>
+        <iframe
+          src="https://share.synthesia.io/embeds/videos/5b83a441-7de4-4238-8ca0-63ffb50eb72a"
+          loading="lazy"
+          title="Synthesia video player - Your AI video"
+          allow="encrypted-media; fullscreen;"
+          style={iframeStyle}
+        ></iframe>
+      </div>
+      <div style={legendStyle}>
+        Hun gir de beste klemmene og alltid er nær, Hun gir deg kjærlighet,
+        omsorg og beskyttelse så kjær. Hun hjelper deg når du faller og trøster
+        deg når du gråter, Hun lager de beste måltidene, og når du er syk, så
+        passer hun på deg hele natten. Hvem er denne personen så god og så varm,
+        Som alltid vil være der for deg, og som aldri vil forsvinne eller gjøre
+        deg harm?
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Form onSubmit={handleSubmit} style={{ width: '50%' }}>
+          <Form.Group controlId="formText">
+            <Form.Control
+              type="text"
+              placeholder="Skriv inn koden her"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              isInvalid={!isValid}
+            />
+            <Form.Control.Feedback type="invalid">
+              Feil kode
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Svar
+          </Button>
+        </Form>
+      </div>
     </>
   );
 };
-
 
 export default CluesPage;
